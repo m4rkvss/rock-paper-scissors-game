129fcddb09b4a8e3dee8c79e5cbb7b9efc2e8368
m4rkvss <114050541+m4rkvss@users.noreply.github.com>
2022-09-28 10:29:17 +0300

    win history added

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 turns = ['rock', 'paper', 'scissors']
 human_turns = []
 computer_turns = []
+win_history = []
 
 while(True):
     human_turn = input("Enter your turn, or type exit: ")
@@ -17,16 +18,22 @@ while(True):
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
         print('Draw!')
+        win_history.append('Draw!')
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
+        win_history.append('Human Wins')
     elif human_turn == 'paper' and computer_turn == 'rock':
         print('Human wins!')
+        win_history.append('Human Wins')
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('Human wins!')
+        win_history.append('Human Wins')
     else:
         print('Computer wins!')
+        win_history.append('Computer Wins!')
 
         
 print(f'You have played {len(human_turns)} times')
 print(human_turns)
 print(computer_turns)
+print(win_history)
